e394160903cd505285069a279bd67ddda973d5a0
pohmx <34806678+pohmx@users.noreply.github.com>
2021-03-29 13:58:31 -0300

    initial

---
+++ osusearchdl.js
@@ -0,0 +1,31 @@
+//osusearch download (osu server)
+$(document).ready(function()  {
+	var time = 3500;
+	var time_0 = 0;
+	var count = 0;
+	var index = 0;
+	var beatmap_limit = 350;
+	document.querySelectorAll(".beatmap-icon-container a:nth-child(2)").forEach(osumap => {
+		count+=1;	
+		if (count <= beatmap_limit){				
+			setTimeout( function(){				
+			index+=1;
+    		console.log('(', index, '/', count, ') downloading: ', $(osumap).attr('href'));
+        	window.open($(osumap).attr('href')).focus();
+        	if (index == beatmap_limit){console.log('finished downloading ヽ(￣д￣;)ノ=3=3=3');}        	  	
+        	}, time_0);    	
+		}
+		time_0 += time;	
+	});
+});
+
+//osu beatset extractor
+$(document).ready(function()  {
+	var count = 0;
+	document.querySelectorAll(".beatmap-icon-container a:nth-child(1)").forEach(osumap => {
+		count+=1;						
+		id = $(osumap).attr('href').replace( /^\D+/g, '').replace(/\/.*/, '');
+		console.log(id);       
+	});
+	console.log('finished extracting (',count,') ヽ(￣д￣;)ノ=3=3=3');
+});
